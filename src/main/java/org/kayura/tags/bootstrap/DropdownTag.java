b5ecba56888b1d0f808ac06ba916b8163b4ff396
liangxia <liangxia@live.com>
2016-05-09 16:49:22 +0800

    bootstrap不错，准备写套支持它的标签库。

---
+++ src/main/java/org/kayura/tags/bootstrap/DropdownTag.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2015-2016 the original author or authors.
+ * HomePage: http://www.kayura.org
+ */
+package org.kayura.tags.bootstrap;
+
+/**
+ * DropdownTag
+ *
+ * @author liangxia@live.com
+ */
+public class DropdownTag {
+
+}
